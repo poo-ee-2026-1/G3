6eae675164812fc559d296563a3a69391e574e03
Victor Giuliano <giulianon10@gmail.com>
2026-04-20 09:29:23 -0300

    3ª Entrega (20/04) Funcionalidade - cadastro de livros
    Added functionality to list and add books in the library.

--- SistemaBiblioteca/src/Biblioteca/Biblioteca.java
+++ SistemaBiblioteca/src/Biblioteca/Biblioteca.java
@@ -6,28 +6,33 @@ import java.util.Scanner;
 
 public class Biblioteca {
 
-	public static void main(String[] args) {
-		
-		Scanner scanner = new Scanner(System.in);
-		ArrayList<Livro> livros = new ArrayList<>();
-		Serviços serviços = new Serviços();
-		boolean parar = false;
-		
-		do {
-				int opcao = serviços.opcao(scanner);
-				
-				switch (opcao) {
-	            case 1:
-	            	break;
-	            case 6:
-	            	parar = true;
-	            	System.out.println("Encerrando...");
-	            }
-				
-		}	while (!parar);
-		
-		scanner.close();
-	}
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Livro> livros = new ArrayList<>();
+        Serviços serviços = new Serviços();
+        boolean parar = false;
+        int id_livro = 1;
+
+        do {
+            int opcao = serviços.opcao(scanner);
+
+            switch (opcao) {
+                case 1:
+                    serviços.listarLivros(livros);
+                    break;
+                case 2:
+                    livros.add(serviços.adicionarLivro(id_livro));
+                    id_livro++;
+                    break;
+                case 6:
+                    parar = true;
+                    System.out.println("Encerrando...");
+            }
+
+        }	while (!parar);
+
+        scanner.close();
+    }
 
 }
-
